97c847df254bcaeeeaf6495347cf8333c62dc783
Guruhg19 <guruhvitra19@gmail.com>
2025-04-08 12:32:25 +0700

    Add checkout, detail, and landing pages with routing and styling updates
    - Created checkout.blade.php for the checkout process with form handling.
    - Added detail.blade.php to display car details and similar cars.
    - Implemented landing.blade.php for the homepage showcasing popular cars and features.
    - Updated web.php to include routes for landing, detail, and checkout pages.
    - Adjusted tailwind.config.js for consistent styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
 			},
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-			    poppins: "Poppins, sans-serif",
+			poppins: "Poppins, sans-serif",
             },
         },
     },
